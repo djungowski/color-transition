483a7586d5350eaf598ca5e44daa9bf22a2323c0
Dominik Jungowski <Dominik@Jungowski.de>
2015-02-16 00:16:40 +0100

    run color transition

--- javascript/color-transition.js
+++ javascript/color-transition.js
@@ -1,4 +1,19 @@
 window.addEventListener('load', function () {
+  var updateColor = function (color) {
+    document.body.style.background = color;
+  };
+
   var color = new Color();
-  document.body.style.background = color.toRgbaString();
+  updateColor(color.toRgbaString());
+
+  var targetColor = {
+    red: 39,
+    green: 166,
+    blue: 176
+  };
+
+  color.fadeInto(targetColor, function () {
+    console.log(color.toRgbaString());
+    updateColor(color.toRgbaString());
+  });
 });
